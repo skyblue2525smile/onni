10b81b908b48815d833167ba88e4f87ed66e4d60
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-08-28 13:03:02 +0000

    [Add]退会機能の実装

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,27 +1,24 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  protected
+  # 退会しているかを判断するメソッド
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    # 【処理内容1】 入力されたemailからアカウントを1件取得
+    return if !@customer
+    # アカウントを取得できなかった場合、このメソッドを終了する
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+    if @customer.valid_password?(params[:customer][:password]) && !@customer.is_deleted
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+    else redirect_to new_customer_registration_path
 
-  # protected
+    end
+    # 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致しているかを判別
+    # 【処理内容3】is_deletedの値がtrueだった場合；サインアップ画面に遷移する処理を実行する
+                  # is_deletedの値がfalseだった場合;createアクションを実行させる
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
