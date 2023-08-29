05f928f6330f5df50b79fbed83c4aa9b20c671ec
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-08-29 10:22:25 +0000

    [Add]動的ヘッダーの作成

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -7,7 +7,7 @@ class Public::SessionsController < Devise::SessionsController
   # 退会しているかを判断するメソッド
   def customer_state
     @customer = Customer.find_by(email: params[:customer][:email])
-    # 【処理内容1】 入力されたemailからアカウントを1件取得
+    # 【処理内容1】 ログイン画面で入力されたemailからCustomerモデルに同じemailのアカウントが存在するかを検索
     return if !@customer
     # アカウントを取得できなかった場合、このメソッドを終了する
 
@@ -16,9 +16,10 @@ class Public::SessionsController < Devise::SessionsController
     else redirect_to new_customer_registration_path
 
     end
-    # 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致しているかを判別
-    # 【処理内容3】is_deletedの値がtrueだった場合；サインアップ画面に遷移する処理を実行する
-                  # is_deletedの値がfalseだった場合;createアクションを実行させる
+    # 【処理内容2】「1」のアカウントが存在している場合、取得したアカウントのパスワードと入力されたパスワードが一致しているかを判別
+    # 【処理内容3】「1」と「2」の処理が真(true)だった場合、そのアカウントのis_deletedカラムに格納されている値を確認し
+    #                is_deletedの値がtrue(退会している)の場合；サインアップ画面に遷移する処理を実行する
+    #                is_deletedの値がfalse(退会していない)の場合;createアクション(ログイン処理)を実行させる
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destory
   has_many :order_details, dependent: :destory
 
+
 end
