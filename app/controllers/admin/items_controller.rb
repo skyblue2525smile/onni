9ba0e0cb98c63401893deecad7d9259c75a1f44d
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-08-31 09:28:16 +0000

    [Add]配送先機能追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,42 @@
 class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.page(params[:page])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      @items = Item.page(params[:page])
+      render :index
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :genre_id, :is_active)
+  end
 end
+
