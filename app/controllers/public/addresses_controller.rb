022224a2fea59f6dfae769e0178d102062e3a15d
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-08-31 11:01:51 +0000

    [Add]カート機能追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,44 @@
 class Public::AddressesController < ApplicationController
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path
+    else
+      @addresses = current_customer.addresses
+      render 'index'
+    end
+  end
+
+  def index
+    @address = Address.new
+    @addresses = current_customer.addresses
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    @address = current_customer.addresses
+    redirect_to addresses_path
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:address_name, :address, :postal_code)
+  end
+
 end
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,46 @@
 class Public::CartItemsController < ApplicationController
+  def index
+    @cart_items = current_customer.cart_items
+    @sum = 0
+  end
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_items_path
+    else
+      @item = Item.find(params[:id])
+      render item_path(@item.id)
+    end
+  end
+
+  def destroy_all
+    CartItem.destroy_all
+    redirect_to cart_items_path
+  end
+
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
+  end
+
+  def create
+    cart_item = current_customer.cart_items.find_by(item_id: cart_item_params[:item_id])
+      if cart_item.present?
+        total_amount = cart_item.amount + cart_item_params[:amount].to_i
+        cart_item.update_attribute(:amount, total_amount)
+      else
+        cart_item_new = CartItem.new(cart_item_params)
+        cart_item_new.save
+
+      end
+       redirect_to cart_items_path
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount, :customer_id)
+  end
 end
+
