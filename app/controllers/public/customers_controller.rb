105df08752234dfe4d60ea177a257742c930d121
skyblue2525smile <130109663+skyblue2525smile@users.noreply.github.com>
2023-08-31 04:17:40 +0000

    [Add]会員管理機能追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,34 @@
 class Public::CustomersController < ApplicationController
+  def show
+   @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_path
+    else
+      render 'edit'
+    end
+  end
+
+  def confirm
+    @customer = Customer.find(current_customer.id)
+  end
+
+  def withdrawal
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+   def customer_params
+    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+   end
 end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -63,4 +63,4 @@ end
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-end
+
